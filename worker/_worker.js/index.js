b4de7f43a3802ceb700d1c3d32c0e64037ee5214
ajmal1212 <87862969+ajmal1212@users.noreply.github.com>
2025-06-19 07:00:47 +0000

    correct

--- worker/_worker.js/index.js
+++ worker/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_9Ni-0hQK.mjs';
-import { manifest } from './manifest_Da39cduI.mjs';
+import { manifest } from './manifest_DJ0I_4mf.mjs';
 
 const serverIslandMap = new Map();;
 
